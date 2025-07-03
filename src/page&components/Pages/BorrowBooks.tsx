ad6ca7485b015da95403174c0ee696addfefd22d
Masudur400 <md8004210@gmail.com>
2025-07-03 21:40:08 +0600

    single borrow cart design

--- src/page&components/Pages/BorrowBooks.tsx
+++ src/page&components/Pages/BorrowBooks.tsx
@@ -1,19 +1,20 @@
 import { selectBorrows } from "@/redux/features/borrowSlice";
 import { useAppSelector } from "@/redux/hook";
+import SingleBorrowBook from "./components/SingleBorrowBook";
 
- 
+
 const BorrowBooks = () => {
 
-     const borrows = useAppSelector(selectBorrows)
+    const borrows = useAppSelector(selectBorrows)
 
     return (
         <div>
             <p className="text-2xl mb-2 font-medium">Borrow Summary</p>
             <p className="text-lg font-medium">Total <span className="text-red-500">({borrows.length})</span> books you have borrowed</p>
-            <div> 
+            <div className="mt-10 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
                 {
-                borrows.map(borrow => <p>{borrow.buyerName}</p>)
-            }
+                    borrows.map(borrow => <SingleBorrowBook key={borrow.id} borrow={borrow} ></SingleBorrowBook>)
+                }
             </div>
         </div>
     );
---
+++ src/page&components/Pages/components/SingleBorrowBook.tsx
@@ -0,0 +1,29 @@
+import { Card, CardContent } from "@/components/ui/card";
+import type { IBorrow } from "@/redux/interfaces/interface";
+
+
+
+interface IProps {
+    borrow: IBorrow;
+}
+
+const SingleBorrowBook = ({ borrow }: IProps) => {
+
+    const { id, bookId, buyerName, quantity, bookTitle, bookAuthor, genre, } = borrow
+
+    return (
+        <Card className="mb-5 flex flex-col h-full">
+            <CardContent className="text-sm space-y-2 h-full">
+                <div>
+                    <h1 className="text-xl font-medium mb-2"> {bookTitle}</h1>
+                    <h1 className="mb-1"><strong>BuyerName : {buyerName}</strong></h1>
+                    <p><strong>Author:</strong> {bookAuthor}</p>
+                    <p><strong>Genre:</strong> {genre}</p>
+                    <p><strong>Quantity:</strong> {quantity}</p>
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default SingleBorrowBook;
